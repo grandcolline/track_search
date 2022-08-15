71e8e76a538ad5f75a3adc3f7892287467d06855
Taiki Hayashi <grandcolline@gmail.com>
2022-08-16 01:44:05 +0900

    🚀

--- adapter/log/simple/src/lib.rs
+++ adapter/log/simple/src/lib.rs
@@ -1,23 +1,38 @@
 use port::log::Log;
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd)]
+pub enum Level {
+    Debug,
+    Info,
+    Error,
+}
+
 #[derive(Debug, Clone)]
 pub struct Logger {
     id: String,
+    level: Level,
 }
 
 impl Logger {
-    pub fn new(s: String) -> Self {
-        Self { id: s }
+    pub fn new(id: String, level: Level) -> Self {
+        Self {
+            id,
+            level,
+        }
     }
 }
 
 impl Log for Logger {
     fn debug(&self, s: String) {
-        println!("\x1b[47m\x1b[45m DEBUG \x1b[m {} | {}", self.id, s);
+        if self.level <= Level::Debug {
+            println!("\x1b[47m\x1b[45m DEBUG \x1b[m {} | {}", self.id, s);
+        }
     }
 
     fn info(&self, s: String) {
-        println!("\x1b[47m\x1b[44m INFO  \x1b[m {} | {}", self.id, s);
+        if self.level <= Level::Info {
+            println!("\x1b[47m\x1b[44m INFO  \x1b[m {} | {}", self.id, s);
+        }
     }
 
     // fn warn(&self, s: String) {
@@ -25,6 +40,8 @@ impl Log for Logger {
     // }
 
     fn error(&self, s: String) {
-        println!("\x1b[47m\x1b[41m ERROR \x1b[m {} | {}", self.id, s);
+        if self.level <= Level::Error {
+            println!("\x1b[47m\x1b[41m ERROR \x1b[m {} | {}", self.id, s);
+        }
     }
 }
--- driver/src/config/log.rs
+++ driver/src/config/log.rs
@@ -1,10 +1,10 @@
 use std::sync::Arc;
 
 use port::LogContainer;
-use simple::Logger;
+use simple::{ Logger, Level };
 
 pub fn init() -> LogContainer {
     LogContainer {
-        log: Arc::new(Logger::new("xxxxxxxx".into())),
+        log: Arc::new(Logger::new("xxxxxxxx".into(), Level::Debug)),
     }
 }
